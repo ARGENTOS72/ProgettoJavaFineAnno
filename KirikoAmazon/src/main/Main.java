5929d83e36bdf87d4a1b1101b4f622bba402183d
JonnyArm <130648163+JonnyArm@users.noreply.github.com>
2024-04-08 12:48:22 +0100

    Add files via upload

--- KirikoAmazon/src/main/Main.java
+++ KirikoAmazon/src/main/Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+import view.Finestra;
+
 public class Main {
 
+	static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+	static int width, height;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Finestra f = new Finestra();
+		
 	}
 
 }
