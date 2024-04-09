6f48fbb5c9358b661765925e89b33ed0bb1445db
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-09 09:52:51 +0100

    maceira perchè mi fai fare ste cose

---
+++ KirikoAmazon/src/model/Loader.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.concurrent.Semaphore;
+
+public class Loader implements Runnable {
+	public static final String init = "init";
+	private Semaphore s;
+	private Thread t;
+	
+	public Loader(String loadingType, Semaphore s) {
+		t = new Thread(this, "Loader");
+		this.s = s;
+	}
+
+
+
+	public void start() {
+		t.start();
+	}
+
+
+
+	@Override
+	public void run() {
+		//to load
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//notify finish
+		
+	}
+}
