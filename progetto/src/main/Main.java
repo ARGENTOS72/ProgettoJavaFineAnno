aee4a64df47af4de822a97c62502da5d3175fd03
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-13 00:49:50 +0200

    Update Main.java with game loop in it
    Update Main.java with game loop in it to permit usage of controller

--- progetto/src/main/Main.java
+++ progetto/src/main/Main.java
@@ -1,13 +1,16 @@
 package main;
 
-import com.raylib.java.Raylib;
-import com.raylib.java.core.Color;
-import com.raylib.java.core.rCore;
-
+import controller.Controller;
 import view.Finestra;
 
 public class Main {
     public static void main(String[] args) {
         Finestra finestra = new Finestra();
+        Controller controller = new Controller(finestra.getPannello());
+        
+        while(!Finestra.getRaylib().core.WindowShouldClose()) {
+        	finestra.draw();
+        	controller.update();
+        }
     }
 }
