221d7db7b8dfaf957e60224ebbdc8b55a634aee6
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-13 00:56:18 +0200

    Update Button.java
    but the Button class is just a Label

--- progetto/src/view/Button.java
+++ progetto/src/view/Button.java
@@ -1,15 +1,13 @@
 package view;
 
-import com.raylib.java.Raylib;
 import com.raylib.java.core.Color;
 import com.raylib.java.raymath.Vector2;
 import com.raylib.java.shapes.Rectangle;
 import com.raylib.java.shapes.rShapes;
-import com.raylib.java.text.rText;
 
-public class Button {
+public class Button extends GraphicComponent {
 	//border
-	private Vector2 border;
+	private Rectangle border;
 	private Color backgroundColor;
 	private boolean borderVisible;
 	private int padding;
@@ -19,8 +17,9 @@ public class Button {
 	private Color foregroundColor;
 	private int fontSize;
 	
-	//Constructor -----------------------------------------
-	public Button(Raylib ray, Color backgroundColor, boolean borderVisible, int padding, String text, Color foregroundColor, int fontSize) {
+	//Constructors -----------------------------------------
+	public Button(Color backgroundColor, boolean borderVisible, int padding, int x, int y, String text, Color foregroundColor, int fontSize) {
+		super();
 		this.backgroundColor = backgroundColor;
 		this.borderVisible = borderVisible;
 		this.padding = padding;
@@ -28,28 +27,32 @@ public class Button {
 		this.foregroundColor = foregroundColor;
 		this.fontSize = fontSize;
 		
-		border = new Vector2(ray.text.MeasureText(text, fontSize)+(padding*2), 0);
+		border = new Rectangle(x, y, Finestra.getRaylib().text.MeasureText(text, fontSize)+(padding*2), fontSize+(padding*2));
 	}
 	
-	public Button(Raylib ray, String text, int fontSize, int padding) {
-		this(ray, Color.WHITE, true, padding, text, Color.BLACK, fontSize);
+	public Button(Color backgroundColor, boolean borderVisible, int padding, Vector2 pos, String text, Color foregroundColor, int fontSize) {
+		this(backgroundColor, borderVisible, padding, (int)(pos.x), (int)(pos.y), text, foregroundColor, fontSize);
 	}
 	
-	public Button(Raylib ray, String text, int fontSize) {
-		this(ray, Color.WHITE, true, 0, text, Color.BLACK, fontSize);
+	public Button(String text, int fontSize, int padding, int x, int y) {
+		this(Color.WHITE, true, padding, new Vector2(x, y), text, Color.BLACK, fontSize);
 	}
 	
-	public Button(Raylib ray, int padding) {//TODO: getRaylibInstance() per favore giacomo pasqua arriva da me!! ( T_T )
-		this(ray, Color.WHITE, true, padding, "", Color.BLACK, 12);
+	public Button(String text, int fontSize, int x, int y) {
+		this(Color.WHITE, true, 0, x, y, text, Color.BLACK, fontSize);
+	}
+	
+	public Button(int padding, int x, int y) {
+		this(Color.WHITE, true, padding, x, y, "", Color.BLACK, 12);
 	}
 	
 	//Draw -----------------------------------------------------------------
-	public void draw(Raylib ray, int x, int y) {
-		if(borderVisible) ray.shapes.DrawRectangle(x, y, (int)(border.x), (int)(border.y), backgroundColor);
-		ray.text.DrawText(text, x+padding, y+padding, fontSize, foregroundColor);
+	public void draw() {
+		if(borderVisible) rShapes.DrawRectangleRec(border, backgroundColor);
+		Finestra.getRaylib().text.DrawText(text, (int)(border.x)+padding, (int)(border.y)+padding, fontSize, foregroundColor);
 	}
 	
-	// ----------------------------------------------------------------
+	//getters + setters + superclass overrides ------------------------------------------
 	public Color getBackgroundColor() {
 		return backgroundColor;
 	}
@@ -71,6 +74,10 @@ public class Button {
 	}
 
 	public void setPadding(int padding) {
+		//recalc border
+		border.width = (border.width-(this.padding*2))+(padding*2);
+		border.height = fontSize+padding+padding;
+		
 		this.padding = padding;
 	}
 
@@ -80,6 +87,9 @@ public class Button {
 
 	public void setText(String text) {
 		this.text = text;
+		
+		//recalc rectangle
+		border.width = Finestra.getRaylib().text.MeasureText(text, fontSize)+(padding*2);
 	}
 
 	public Color getForegroundColor() {
@@ -96,5 +106,22 @@ public class Button {
 
 	public void setFontSize(int fontSize) {
 		this.fontSize = fontSize;
+		
+		//recalc rectangle
+		border.width = Finestra.getRaylib().text.MeasureText(text, fontSize)+(padding*2);
+		border.height = fontSize+padding+padding;
+	}
+	
+	public Rectangle getBorder() {
+		return border;
+	}
+	
+	public void setBorder(Rectangle border) {
+		this.border = border;
+	}
+	
+	@Override
+	public boolean isHovered(Vector2 mousePos) {
+		return Finestra.getRaylib().shapes.CheckCollisionPointRec(mousePos, border);
 	}
 }
