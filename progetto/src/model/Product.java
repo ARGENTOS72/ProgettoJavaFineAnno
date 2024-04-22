0e1f759afd5a654fcf78ec4d069bf59e1d3d3072
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-23 00:43:12 +0200

    Update Product.java

--- progetto/src/model/Product.java
+++ progetto/src/model/Product.java
@@ -2,18 +2,78 @@ package model;
 
 import java.io.Serializable;
 
+import com.raylib.java.textures.Texture2D;
+
 public class Product implements Serializable {
-    private int code;
-    private String name;
-    private int quantity;
+	private double prezzo;
+	private String nome, descrizione;
+	private int quantita;
+	private int codice;
+	
+	//Constructor -----------------------------------------------
+	public Product(double prezzo, String nome, String descrizione, int quantita, int codice) {
+		super();
+		this.prezzo = prezzo;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.quantita = quantita;
+		this.codice = codice;
+	}
+	
+	public Product(double prezzo, String nome, String descrizione, int quantita) {
+		super();
+		this.prezzo = prezzo;
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.quantita = quantita;
+	}
 
-    public Product(int code, String name, int quantity) {
-        this.code = code;
-        this.name = name;
-        this.quantity = quantity;
-    }
+    //getters & setters -------------------------------------------------
+	public double getPrezzo() {
+		return prezzo;
+	}
 
-    public int getCode() {
-        return this.code;
-    }
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public int getQuantita() {
+		return quantita;
+	}
+
+	public int getCodice() {
+		return codice;
+	}
+
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public void setQuantita(int quantita) {
+		this.quantita = quantita;
+	}
+
+	public void setCodice(int codice) {
+		this.codice = codice;
+	}
+
+	//toSting ---------------------------------
+	@Override
+	public String toString() {
+		return "Product [prezzo=" + prezzo + ", nome=" + nome + ", descrizione=" + descrizione
+				+ ", quantita=" + quantita + ", codice=" + codice + "]";
+	}
+
 }
