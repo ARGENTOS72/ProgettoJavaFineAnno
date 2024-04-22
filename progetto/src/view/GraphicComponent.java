aaa49c0dcd3496213b68973c1e358358d9e6dcb7
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-23 00:45:39 +0200

    Update GraphicComponent.java
    for listener: renamed prev in addListener & added removeListener

--- progetto/src/view/GraphicComponent.java
+++ progetto/src/view/GraphicComponent.java
@@ -2,6 +2,8 @@ package view;
 
 import com.raylib.java.raymath.Vector2;
 
+import controller.Controller;
+
 public class GraphicComponent {// has an hitbox, isHovered
 	public static final int PRESSED = 0, DOWN = 1, RELEASED = 2, UP = 3;
 	private String name;
@@ -39,5 +41,15 @@ public class GraphicComponent {// has an hitbox, isHovered
 	//method to check wheter mouse is over the component or not
 	public boolean isHovered(Vector2 mousePos) {
 		return false;
+	}
+	
+	//default operations to do when this GraphicComponent need to add a listener
+	public void addListener(Controller c) {}
+	//default operations to do when this GraphicComponent need to remove a listener
+	public void removeListener(Controller c) {}
+		
+	@Override
+	public String toString() {
+		return name;
 	}
 }
