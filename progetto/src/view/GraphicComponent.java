1584dda8dd8f08ab31a5e567e046acab83f3e6d5
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-25 00:21:07 +0200

    Rework of GraphicComponent

--- progetto/src/view/GraphicComponent.java
+++ progetto/src/view/GraphicComponent.java
@@ -1,55 +1,146 @@
 package view;
 
+import com.raylib.java.core.Color;
 import com.raylib.java.raymath.Vector2;
+import com.raylib.java.shapes.Rectangle;
+import com.raylib.java.textures.Texture2D;
+import com.raylib.java.textures.rTextures;
 
 import controller.Controller;
 
-public class GraphicComponent {// has an hitbox, isHovered
-	public static final int PRESSED = 0, DOWN = 1, RELEASED = 2, UP = 3;
-	private String name;
+public class TextureButton extends Button {
+	private Texture2D texture;
+	private Rectangle textureBounds;
+	private Vector2 origin;
+	private int padding;
 	
-	//Constructor -------------------------------
-	public GraphicComponent() {
-		this.name = null;
+	//Constructor --------------------------------------------
+	public TextureButton(TextureButton textureBtn) {
+		super(textureBtn);
+		setSize(getWidth()+(textureBtn.getPadding()*2), getHeight()+(textureBtn.getPadding()*2));
+		this.padding = textureBtn.getPadding();
+		this.texture = textureBtn.getTexture();
+		this.textureBounds = new Rectangle(0, 0, texture.width, texture.height);
+		this.origin = new Vector2(textureBtn.getOrigin().x, textureBtn.getOrigin().y);
+	}
+	
+	public TextureButton(Button btn, Texture2D texture, int padding, Vector2 origin) {
+		super(btn);
+		setSize(getWidth()+(padding*2), getHeight()+(padding*2));
+		this.padding = padding;
+		this.texture = texture;
+		this.textureBounds = new Rectangle(0, 0, texture.width, texture.height);
+		this.origin = origin;
+	}
+	
+	public TextureButton(Texture2D texture, Rectangle bounds, boolean visible, float roundness, int padding, Color color, Color hoveredColor, Color clickedColor) {
+		super(bounds, visible, roundness, color, hoveredColor, clickedColor);
+		setSize(getWidth()+(padding*2), getHeight()+(padding*2));
+		this.padding = padding;
+		this.texture = texture;
+		this.textureBounds = new Rectangle(0, 0, texture.width, texture.height);
+		this.origin = new Vector2(0, 0);
+	}
+	
+	public TextureButton(Texture2D texture, int x, int y, int width, int height, boolean visible, float roundness, int padding, Color color, Color hoveredColor, Color clickedColor) {
+		super(x, y, width, height, visible, roundness, color, hoveredColor, clickedColor);
+		setSize(getWidth()+(padding*2), getHeight()+(padding*2));
+		this.padding = padding;
+		this.texture = texture;
+		this.textureBounds = new Rectangle(0, 0, texture.width, texture.height);
+		this.origin = new Vector2(0, 0);
+	}
+	
+	public TextureButton(Texture2D texture, int x, int y, int width, int height, boolean visible, float roundness, Color color) {
+		this(texture, x, y, width, height, visible, roundness, 0, null, null, null);
+	}
+	
+	//use only for sponsored product (keep aspect ratio of a wide img)
+	public TextureButton(int x, int y, int width, Texture2D texture) {
+		super(new Rectangle(x, y, width, texture.height*(width / texture.width)), false, 0f, Color.WHITE);
+		this.textureBounds = new Rectangle(0, 0, texture.width, texture.height);
+		this.origin = new Vector2(0, 0);
+		this.padding = 0;
+		this.texture = texture;
+	}
+	
+	//draw ------------------------------------------------------
+	public void draw() {
+		super.draw();
+		rTextures.DrawTexturePro(texture, textureBounds, new Rectangle(getX()+padding, getY()+padding, getWidth()-(padding*2), getHeight()-(padding*2)), origin, 0, Color.WHITE);
+	}
+	
+	//getters & setters ------------------------------------------
+	public Texture2D getTexture() {
+		return texture;
 	}
 
-	public GraphicComponent(String name) {
-		this.name = name;
+	public int getPadding() {
+		return padding;
 	}
 	
-	//getters & setters -------------------------
-	public String getName() {
-		return name;
+	public Vector2 getOrigin() {
+		return origin;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setTexture(Texture2D texture) {
+		this.texture = texture;
+		this.textureBounds.width = texture.width;
+		this.textureBounds.height = texture.height;
+	}
+
+	public void setPadding(int padding) {
+		setSize(getWidth()-(this.padding*2)+(padding*2), getHeight()-(this.padding*2)+(padding*2));
+		this.padding = padding;
 	}
 	
-	//must-have method ---------------------------
-	//default operations to do intra-class when component is on hover
-	public void onHover() {}
-	//default operations to do intra-class when component is out of hover
-	public void outOfHover() {}
-	//default operations to do intra-class when component is on focus
-	public void onFocus() {}
-	//default operations to do intra-class when component is out of focus
-	public void outOfFocus() {}
-	//default operations to do intra-class when component is clicked either pressed, down, released or up
-	public void onClick(int modality) {}
+	public void setOrigin(int x, int y) {
+		this.origin = new Vector2(x, y);
+	}
 	
-	//method to check wheter mouse is over the component or not
+	//superclass overrides ---------------------------------------------
+	@Override
+	public void onHover() {
+		super.onHover();
+	}
+	
+	@Override
+	public void outOfHover() {
+		super.outOfHover();
+	}
+	
+	@Override
+	public void onClick(int modality) {
+		if(modality == GraphicComponent.DOWN) {
+			super.onClick(modality);
+		}
+	}
+	
+	@Override
 	public boolean isHovered(Vector2 mousePos) {
-		return false;
+		return super.isHovered(mousePos);
 	}
 	
-	//default operations to do when this GraphicComponent need to add a listener
-	public void addListener(Controller c) {}
-	//default operations to do when this GraphicComponent need to remove a listener
-	public void removeListener(Controller c) {}
-		
+	@Override
+	public void addListener(Controller c) {
+		c.addListenerTo(this);
+	}
+	
+	@Override
+	public void removeListener(Controller c) {
+		c.removeListenerTo(this);
+	}
+	
+	//toString ---------------------------------------------------
 	@Override
 	public String toString() {
-		return name;
+		return "Button {" +
+				"\n\tbounds [" + getX() + ", " + getY() + ", " + getWidth() + ", " + getHeight() +
+				"]\n\tvisible: " + isVisible() +
+				"\n\troundness: " + getRoundness() +
+				"\n\tcolors [normal: " + getColor() + ", hovered: " + getHoveredColor() + ", clicked: " + getClickedColor() +
+				"]\n\ttexture: " + texture +
+				"\n\tpadding: " + padding +
+				"\n}";
 	}
 }
