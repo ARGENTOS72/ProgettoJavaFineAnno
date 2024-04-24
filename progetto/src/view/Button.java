709bc3a32b5cd94e0f2ed259f737acbf6e311262
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-25 00:20:29 +0200

    Update Button.java due to GraphicComponent rework

--- progetto/src/view/Button.java
+++ progetto/src/view/Button.java
@@ -7,154 +7,63 @@ import com.raylib.java.shapes.Rectangle;
 import controller.Controller;
 
 public class Button extends GraphicComponent {
-	private Rectangle bounds;
 	private boolean visible;
 	private float roundness;
-	private Color color, hoveredColor, clickedColor, currentColor;
 
 	// Constructors -----------------------------------------
 	public Button(Button btn) {
-		super();
-		this.bounds = new Rectangle(btn.getX(), btn.getY(), btn.getWidth(), btn.getHeight());
+		super(new Rectangle(btn.getX(), btn.getY(), btn.getWidth(), btn.getHeight()), null,
+			btn.getColor(), btn.getHoveredColor(), btn.getClickedColor(), null);
+		
 		this.visible = btn.isVisible();
 		this.roundness = btn.getRoundness();
-		this.color = btn.getColor();
-		this.hoveredColor = btn.getHoveredColor();
-		this.clickedColor = btn.getClickedColor();
-		this.currentColor = color;
 	}
 	
 	public Button(Rectangle bounds, boolean visible, float roundness, Color color, Color hoveredColor, Color clickedColor) {
-		super();
-		this.bounds = bounds;
+		super(bounds, null, color, hoveredColor, clickedColor, null);
+		
 		this.visible = visible;
 		this.roundness = roundness;
-		this.color = color;
-		this.hoveredColor = hoveredColor;
-		this.clickedColor = clickedColor;
-		this.currentColor = color;
 	}
 	
 	public Button(int x, int y, int width, int height, boolean visible, float roundness, Color color, Color hoveredColor, Color clickedColor) {
-		super();
-		this.bounds = new Rectangle(x, y, width, height);
-		this.visible = visible;
-		this.roundness = roundness;
-		this.color = color;
-		this.hoveredColor = hoveredColor;
-		this.clickedColor = clickedColor;
-		this.currentColor = color;
+		this(new Rectangle(x, y, width, height), visible, roundness, color, hoveredColor, clickedColor);
 	}
 	
 	public Button(Rectangle bounds, boolean visible, float roundness, Color color) {
-		this(bounds, visible, roundness, color, null, null);
+		super((int) bounds.x, (int) bounds.y, (int) bounds.width, (int) bounds.height, color);
+		
+		this.visible = visible;
+		this.roundness = roundness;
 	}
 	
-	public Button(int x, int y, int width, int height, float roundness, boolean visible, Color color) {
-		this(new Rectangle(x, y, width, height), visible, roundness, color, null, null);
+	public Button(int x, int y, int width, int height, boolean visible, float roundness, Color color) {
+		super(x, y, width, height, color);
+		
+		this.visible = visible;
+		this.roundness = roundness;
 	}
 	
 	public Button(int x, int y, int width, int height, Color color) {
-		this(new Rectangle(x, y, width, height), true, 0, color, null, null);
+		super(x, y, width, height, color);
+		
+		this.visible = true;
+		this.roundness = 0;
 	}
 
 	// Draw -----------------------------------------------------------------
+	@Override
 	public void draw() {
-		if (visible) Finestra.getRaylib().shapes.DrawRectangleRounded(bounds, roundness, 5, currentColor);
+		if(visible) Finestra.getRaylib().shapes.DrawRectangleRounded(getBounds(), roundness, 5, getCurrentColor());
 	}
 
 	// getters + setters ----------------------------------------------------
-	public Rectangle getBounds() {
-		return bounds;
-	}
-	
-	public Vector2 getLocation() {
-		return new Vector2(bounds.x, bounds.y);
-	}
-	
-	public Vector2 getSize() {
-		return new Vector2(bounds.width, bounds.height);
-	}
-	
-	public int getX() {
-		return (int) bounds.x;
-	}
-	
-	public int getY() {
-		return (int) bounds.y;
-	}
-	
-	public int getWidth() {
-		return (int) bounds.width;
-	}
-	
-	public int getHeight() {
-		return (int) bounds.height;
-	}
-
 	public boolean isVisible() {
 		return visible;
 	}
 
 	public float getRoundness() {
 		return roundness;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public Color getHoveredColor() {
-		return hoveredColor;
-	}
-
-	public Color getClickedColor() {
-		return clickedColor;
-	}
-
-	public Color getCurrentColor() {
-		return currentColor;
-	}
-	
-	public Color[] getColors() {
-		return new Color[] {color, hoveredColor, clickedColor};
-	}
-
-	public void setBounds(Rectangle bounds) {
-		this.bounds = bounds;
-	}
-	
-	public void setBounds(int x, int y, int width, int height) {
-		bounds.x = x;
-		bounds.y = y;
-		bounds.width = width;
-		bounds.height = height;
-	}
-	
-	public void setLocation(int x, int y) {
-		bounds.x = x;
-		bounds.y = y;
-	}
-	
-	public void setSize(int width, int height) {
-		bounds.width = width;
-		bounds.height = height;
-	}
-	
-	public void setX(int x) {
-		bounds.x = x;
-	}
-	
-	public void setY(int y) {
-		bounds.y = y;
-	}
-	
-	public void setWidth(int width) {
-		bounds.width = width;
-	}
-	
-	public void setHeight(int height) {
-		bounds.height = height;
 	}
 
 	public void setVisible(boolean visible) {
@@ -164,52 +73,8 @@ public class Button extends GraphicComponent {
 	public void setRoundness(float roundness) {
 		this.roundness = roundness;
 	}
-
-	public void setColor(Color color) {
-		this.color = color;
-		this.currentColor = color;
-	}
-
-	public void setHoveredColor(Color hoveredColor) {
-		this.hoveredColor = hoveredColor;
-	}
-
-	public void setClickedColor(Color clickedColor) {
-		this.clickedColor = clickedColor;
-	}
-	
-	public void setCurrentColor(Color curentColor) {
-		this.currentColor = curentColor;
-	}
-	
-	public void setColors(Color color, Color hoveredColor, Color clickedColor) {
-		this.color = color;
-		this.hoveredColor = hoveredColor;
-		this.clickedColor = clickedColor;
-		this.currentColor = color;
-	}
 	
 	//superclass overrides ---------------------------------------------------
-	@Override
-	public void onHover() {
-		if(hoveredColor != null) currentColor = hoveredColor;
-	}
-	
-	@Override
-	public void outOfHover() {
-		currentColor = color;
-	}
-	
-	@Override
-	public void onClick(int modality) {
-		if(clickedColor != null && modality == GraphicComponent.DOWN) currentColor = clickedColor;
-	}
-	
-	@Override
-	public boolean isHovered(Vector2 mousePos) {
-		return Finestra.getRaylib().shapes.CheckCollisionPointRec(mousePos, bounds);
-	}
-	
 	@Override
 	public void addListener(Controller c) {
 		c.addListenerTo(this);
@@ -227,7 +92,8 @@ public class Button extends GraphicComponent {
 				"\n\tbounds [" + getX() + ", " + getY() + ", " + getWidth() + ", " + getHeight() +
 				"]\n\tvisible: " + visible +
 				"\n\troundness: " + roundness +
-				"\n\tcolors [normal: " + color + ", hovered: " + hoveredColor + ", clicked: " + clickedColor +
+				"\n\tcolors [normal: " + getColor() + ", hovered: " + getHoveredColor() +
+				", clicked: " + getClickedColor() +
 				"]\n}";
 	}
 }
