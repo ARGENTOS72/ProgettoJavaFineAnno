f0039d2378949fbba63e2b84067fe042f81a635b
gappppp <94542946+gappppp@users.noreply.github.com>
2024-04-29 23:08:24 +0100

    Added Listenable & Animable Interfaces

---
+++ progetto/src/view/Listenable.java
@@ -0,0 +1,33 @@
+package view;
+
+import com.raylib.java.raymath.Vector2;
+
+import controller.Controller;
+
+public interface Listenable {
+	public static final int PRESSED = 0, DOWN = 1, RELEASED = 2, UP = 3;
+
+	//default operations to do intra-class when component is on hover
+	public void onHover();
+	
+	//default operations to do intra-class when component is out of hover
+	public void outOfHover();
+	
+	//default operations to do intra-class when component is on focus
+	public void onFocus();
+	
+	//default operations to do intra-class when component is out of focus
+	public void outOfFocus();
+	
+	//default operations to do intra-class when component is clicked either pressed, down, released or up
+	public void onClick(int modality);
+	
+	//method to check wheter mouse is over the component or not
+	public boolean isHovered(Vector2 mousePos);
+	
+	//default operations to do when this Listenable Object need to add a listener
+	public void addListener(Controller c);
+	
+	//default operations to do when this Listenable Object need to remove a listener
+	public void removeListener(Controller c);
+}
