54c482de338f5ff2f9537832e359302a6b6436fe
JonnyArm <130648163+JonnyArm@users.noreply.github.com>
2024-05-01 16:03:31 +0200

    Update HomePage.java Fixed prodottoConsigliato and prodotti position

--- progetto/src/view/HomePage.java
+++ progetto/src/view/HomePage.java
@@ -27,29 +27,31 @@ public class HomePage {
 		super();
 		this.prodottoConsigliatoY = prodottoConsigliatoY;
 		this.prodottoConsigliatoHeight = prodottoConsigliatoHeight;
-		this.prodottoWidth = screenWidth/4;
+		this.prodottoWidth = screenWidth/3;
 		this.screenHeight = screenHeight;
 		this.screenWidth = screenWidth;
 		
 		prodotti = new Prodotto[2][5];
 		
 		texture = new Texture2D("textures/SearchIcon.png");
-		
-		prodottoConsigliato = new TextureButton(texture, 0, prodottoConsigliatoY, screenWidth, prodottoConsigliatoHeight, true, 0f, Color.GOLD);
+
+		prodottoConsigliato = new TextureButton(texture, 0, prodottoConsigliatoY, screenWidth, prodottoConsigliatoHeight, true, 0, 0, Color.LIGHTGRAY, Color.GRAY, Color.BLANK);
 		
 		Product pp = new Product(911.01, "Kiriko 2", "semplice carta das sedere", 0);
 		
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < 2; i++)
+			for(int j = 0; j < 5; j++)
 				prodotti[i][j] = new Prodotto(0, 0, prodottoWidth, 0.4f, texture, pp, 20, 15, 10, Color.VIOLET, Color.DARKPURPLE, Color.PINK);
-			}
-			GraphicComponentAligner.alignY(prodotti[i], GraphicComponentAligner.UP, GraphicComponentAligner.PADDING,
-					(prodottoConsigliatoY+prodottoConsigliatoHeight+screenHeight/30), 10, screenHeight/30);
-		}
 		
+		GraphicComponentAligner.alignY(prodotti[0], GraphicComponentAligner.UP, GraphicComponentAligner.PADDING,
+				(prodottoConsigliatoY+prodottoConsigliatoHeight+screenHeight/20), 10, screenHeight/20);
+		
+		for(int i = 0; i < 5; i++) {
+			prodotti[1][i].setY(prodotti[0][i].getY());
+			GraphicComponentAligner.alignX(new GraphicComponent[] {prodotti[0][i], prodotti[1][i]},
+				GraphicComponentAligner.CENTER, GraphicComponentAligner.DISTRIBUTE, 0, screenWidth,-1);
+		}
 	}
-    
-    
     
     public void draw() {
     	prodottoConsigliato.draw();
@@ -75,4 +77,16 @@ public class HomePage {
     public int getHomePageHeight() {
 		return prodottoConsigliatoHeight;
     }
+
+	public int getProdottoConsigliatoY() {
+		return prodottoConsigliatoY;
+	}
+
+	public int getProdottoConsigliatoHeight() {
+		return prodottoConsigliatoHeight;
+	}
+
+	public int getProdottoWidth() {
+		return prodottoWidth;
+	}
 }
