709fe919db7571c0854fb1261ccdbee97c166e3c
gappppp <94542946+gappppp@users.noreply.github.com>
2024-05-01 17:58:31 +0200

    Update Animable.java (callback update)

--- progetto/src/view/Animable.java
+++ progetto/src/view/Animable.java
@@ -1,14 +1,6 @@
 package view;
 
-import controller.Controller;
-
 public interface Animable {
 	//default operations to do intra-class when component is updated
 	public void update(float deltaTime);
-
-	//default operations to do when this Animable Object need to add a updater
-	public void addUpdater(Controller c);
-	
-	//default operations to do when this Animable Object need to remove a updater
-	public void removeUpdater(Controller c);
 }
