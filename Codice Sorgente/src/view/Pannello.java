62ae88f17454efc6f7298fc88ad73f2cefe25b23
gappppp <94542946+gappppp@users.noreply.github.com>
2024-05-05 22:09:24 +0200

    Update Pannello.java

--- Codice Sorgente/src/view/Pannello.java
+++ Codice Sorgente/src/view/Pannello.java
@@ -3,11 +3,9 @@ package view;
 import java.util.ArrayList;
 
 import com.raylib.java.Raylib;
-import com.raylib.java.core.rCore;
 import com.raylib.java.core.Color;
 import com.raylib.java.core.camera.Camera2D;
 import com.raylib.java.raymath.Vector2;
-import com.raylib.java.textures.Texture2D;
 
 import controller.Controller;
 import model.Product;
@@ -15,67 +13,72 @@ import model.Product;
 public class Pannello {
     private Raylib ray;
 
+    private int screenWidth, screenHeight;
+    private Camera2D camera;
+    private int maxCameraHeight;
+    
     private LoadingView loadingView;
     private Header header;
     private HomePage homePage;
     private ProductView productView;
+    private Footer footer;
     private ProductsSearched productsSearched;
-    
-    private Texture2D texture;
-    private int screenWidth;
-    private int screenHeight;
-    private Camera2D camera;
 
     private CurrentView currentView;
     
     public Pannello() {
     	//init vars
         this.ray = Finestra.getRaylib();
-        screenWidth = rCore.GetScreenWidth();
-        screenHeight = rCore.GetScreenHeight();
-    
-        texture = new Texture2D("textures/SearchIcon.png");
+        screenWidth = Finestra.unscaledScreenWidth;
+        screenHeight = Finestra.unscaledScreenHeight;
         
+        //init views
         loadingView = new LoadingView(screenWidth, screenHeight);
         header = new Header(screenWidth, screenHeight, screenWidth, screenHeight/5, screenWidth/30, screenHeight/14, 6);
         homePage = new HomePage(screenWidth, screenHeight, header.getHeight(), screenHeight/2, 10);
-        productView = new ProductView(texture, null, screenWidth, screenHeight, header.getHeight());
+        footer = new Footer(homePage.getHeight(), screenWidth, screenHeight);
+        productView = new ProductView(screenWidth, screenHeight, header.getHeight());
         productsSearched = new ProductsSearched(header.getHeight(), screenWidth, screenHeight);
        
-	    camera = new Camera2D(new Vector2(0, 0), new Vector2(0, 0), 0f, 1f);
-        
         currentView = CurrentView.HomePage;
+        
+        //init camera
+	    camera = new Camera2D(new Vector2(0, 0), new Vector2(0, 0), 0f, 1f);
+	    maxCameraHeight = footer.getHeight() - screenHeight;
     }
     
     //Draw Panel
     public void draw() {
     	ray.core.BeginMode2D(camera);
-	Finestra.getRaylib().core.ClearBackground(new Color(141, 255, 248, 255));
-	    
-    	//loadingView.draw();
-    	header.draw();
+    	Finestra.getRaylib().core.ClearBackground(new Color(141, 255, 248, 255));
 
         switch (currentView) {
-            case CurrentView.Loading: {
+            case Loading: {
                 loadingView.draw();
                 
                 break;
             }
 
-            case CurrentView.HomePage: {
+            case HomePage: {
+            	header.draw();
                 homePage.draw();
+                footer.draw();
                 
                 break;
             }
 
-            case CurrentView.ProductView: {
+            case ProductView: {
+            	header.draw();
                 productView.draw();
+                footer.draw();
                 
                 break;
             }
 
-            case CurrentView.ProductsSearched: {
+            case ProductsSearched: {
+            	header.draw();
                 productsSearched.draw();
+                footer.draw();
 
                 break;
             }
@@ -89,20 +92,31 @@ public class Pannello {
 
         currentView = CurrentView.ProductView;
 
-        productView.registraEventiProdotto(c);
-        homePage.rimuoviEventiHome(c);
-        productsSearched.rimuoviEventiProductsSearched(c);
+        productView.registraEventi(c);
+        homePage.rimuoviEventi(c);
+        productsSearched.rimuoviEventi(c);
 
+        footer.setOffsetY(productView.getHeight());
+        maxCameraHeight = footer.getHeight() - screenHeight;
+        
         camera.target.y = 0;
+        
+        if(prodotto.getQuantita() == 0) {
+        	
+//        	new TextButton(x, y, false, 0f, Color.YELLOW, null, null, 0, 30, "No", Color.BLACK, null, null);
+        }
     }
 
     public void showHomePage(Controller c) {
         currentView = CurrentView.HomePage;
 
-        productView.rimuoviEventiProdotto(c);
-        homePage.registraEventiHome(c);
-        productsSearched.rimuoviEventiProductsSearched(c);
+        productView.rimuoviEventi(c);
+        homePage.registraEventi(c);
+        productsSearched.rimuoviEventi(c);
     
+        footer.setOffsetY(homePage.getHeight());
+        maxCameraHeight = footer.getHeight() - screenHeight;
+        
         camera.target.y = 0;
     }
 
@@ -111,10 +125,13 @@ public class Pannello {
 
         currentView = CurrentView.ProductsSearched;
 
-        productView.rimuoviEventiProdotto(c);
-        homePage.rimuoviEventiHome(c);
-        productsSearched.registraEventiProductsSearched(c);
+        productView.rimuoviEventi(c);
+        homePage.rimuoviEventi(c);
+        productsSearched.registraEventi(c);
 
+        footer.setOffsetY(productsSearched.getHeight());
+        maxCameraHeight = footer.getHeight() - screenHeight;
+        
         camera.target.y = 0;
     }
 
@@ -126,6 +143,7 @@ public class Pannello {
     public void aggiornaCameraY(float y) {
 		camera.target.y -= y;
 		camera.target.y = Math.max(0, camera.target.y);
+		camera.target.y = Math.min(maxCameraHeight, camera.target.y);
     }
     
     public Camera2D getCamera() {
@@ -135,15 +153,14 @@ public class Pannello {
     public void loadHomeProducts(ArrayList<Product> products) {
         homePage.loadHomeProducts(products);
     }
-
+    
+	public int getHomePageNProdotti() {
+	        return homePage.getNProdotti();
+	}
+	
     public void registraEventi(Controller c) {
        	header.registraEventiHeader(c);
-        homePage.registraEventiHome(c);
-        //productView.registraEventiProdotto(c);
-    	//loadingView.registraEventi(c);
-    }
-
-    public int getHomePageNProdotti() {
-        return homePage.getNProdotti();
-    }
+        homePage.registraEventi(c);
+        footer.registraEventiFoot(c);
+    }    
 }
